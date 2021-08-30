a05f89086319d7eea8bef5b05eaa2aab300275ff
woodm1979 <woodm1979@gmail.com>
2021-08-30 08:40:49 -0600

    primes works up to 2

--- src/primes.rs
+++ src/primes.rs
@@ -1,19 +1,40 @@
-// use crate::incrementer;
-//
-// struct Primes {
-//     sieve_pieces: Vec<Tuple<u64, incrementer::Incrementer>>,
-// }
-//
-// impl Primes {
-//     fn new() -> Primes {
-//         Primes {
-//             sieve_pieces: [].to_vec(),
-//         }
-//     }
-// }
-//
-// impl Iterator for Primes {
-//     type Item = u64;
-//
-//     fn next(&mut self) -> Option<Self::Item> {}
-// }
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use crate::incrementer;
+
+struct Primes {
+    cursor: u64,
+    // sieve_pieces: BinaryHeap<Tuple<u64, incrementer::Incrementer>>,
+}
+
+impl Primes {
+    fn new() -> Primes {
+        Primes {
+            cursor: 1,
+            // sieve_pieces: BinaryHeap::new(),
+        }
+    }
+}
+
+impl Iterator for Primes {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cursor < 2 {
+            self.cursor += 1;
+            return Some(self.cursor);
+        }
+        return None;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::primes::Primes;
+    #[test]
+    fn it_works() {
+        let max: u64 = 3;
+        let primes_below_max: Vec<_> = Primes::new().take_while(|i| i < &max).collect();
+        assert_eq!(primes_below_max, [2])
+    }
+}

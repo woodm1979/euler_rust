4b9eb7843aad21315f6b5ae71c96b11ee292f88a
woodm1979 <woodm1979@gmail.com>
2021-09-05 07:41:21 -0600

    refactor euler_001.rs

--- src/bin/euler_001.rs
+++ src/bin/euler_001.rs
@@ -11,27 +11,21 @@
 //! user	0m0.001s
 //! sys	0m0.001s
 //! ```
+
 use std::collections::HashSet;
 
-fn get_multiples(multiplier: usize, max: &usize) -> HashSet<usize> {
-    return (multiplier..*max).step_by(multiplier).collect();
+fn get_multiples(multiplier: u64, max: &u64) -> HashSet<u64> {
+    return (multiplier..*max).step_by(multiplier as usize).collect();
 }
 
-fn euler_001(max: usize) {
+fn euler_001(max: u64) -> u64 {
     let by_3 = get_multiples(3, &max);
     let by_5 = get_multiples(5, &max);
     let by_3_or_5 = by_3.union(&by_5);
-
-    // let mut by_3_or_5: Vec<_> = by_3_or_5.into_iter().collect();
-    // by_3_or_5.sort();
-    // for i in &by_3_or_5 {
-    //     println!("{}", i)
-    // }
-
-    let sum: usize = by_3_or_5.into_iter().sum();
-    println!("Sum: {}", sum)
+    by_3_or_5.into_iter().sum()
 }
 
 fn main() {
-    euler_001(1000)
+    let sum = euler_001(1000);
+    println!("Sum: {}", sum);
 }
